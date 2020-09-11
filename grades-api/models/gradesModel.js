d415a3ce4a27647d367db6fa79c285a602eacd7c
julianocf <julianocf.2009@gmail.com>
2020-09-10 21:11:09 -0300

    first commit

---
+++ grades-api/models/gradesModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const gradesSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  subject: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  value: {
+    type: Number,
+    required: true,
+  },
+  lastModified: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const gradesModel = mongoose.model('grades', gradesSchema);
+
+export { gradesModel };
